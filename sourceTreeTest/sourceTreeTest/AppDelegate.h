26f52620fe7a4fefdc6697ac3cd45bec1318421e
anita <anita@anitadeMacBook-Air.local>
2017-07-06 21:33:24 +0800

    创建

---
+++ sourceTreeTest/sourceTreeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sourceTreeTest
+//
+//  Created by anita on 2017/7/6.
+//  Copyright © 2017年 anitaAnitaAnita赵国瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sourceTreeTest/sourceTreeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sourceTreeTest
+//
+//  Created by anita on 2017/7/6.
+//  Copyright © 2017年 anitaAnitaAnita赵国瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
